3a9387c18fe10a36e3769e38a08a5568623fffaf
Pedro Carvajal <pscarvajal.24@est.ucab.edu.ve>
2026-03-28 18:34:42 -0400

    refactor: si

--- chunkLoader.h
+++ chunkLoader.h
@@ -93,100 +93,9 @@ public:
         paintPool.clear();
     }
 
-    void loadChunk(GLFWwindow *window, Camera &cam) {
-        float escala = 45.0f;
-        int cellx = (int)floor(cam.position.x / escala);
-        int cellz = (int)floor(cam.position.z / escala);
-        int radio = 2;
-
-        static int lastCellX = -99999, lastCellZ = -99999;
-        if (lastCellX != -99999) {
-            if (cellx > lastCellX) {
-                 int badX = lastCellX + radio;
-                 for (auto it = mundialMap.begin(); it != mundialMap.end(); ) {
-                    if (it->first.x == badX) {
-                        returnPaint(it->second);  
-                        if (dataMap.count(it->first)) {
-                            returnModel(dataMap[it->first]);  
-                            dataMap.erase(it->first);
-                        }
-                        it = mundialMap.erase(it);
-                    } else ++it;
-                 }
-            }
-            if (cellz > lastCellZ) {
-                 int badZ = lastCellZ + radio;
-                 for (auto it = mundialMap.begin(); it != mundialMap.end(); ) {
-                    if (it->first.z == badZ) {
-                        returnPaint(it->second); 
-                        if (dataMap.count(it->first)) {
-                            returnModel(dataMap[it->first]);
-                            dataMap.erase(it->first);
-                        }
-                        it = mundialMap.erase(it);
-                    } else ++it;
-                 }
-            }
-        }
-        lastCellX = cellx; lastCellZ = cellz;
-
-        std::map<Coordenada, Model*> tempModels;
-
-        for (int x = cellx - radio; x <= cellx + radio; x++) {
-            for (int z = cellz - radio; z <= cellz + radio; z++) {
-                Coordenada c = {x, z};
-
-                if (dataMap.find(c) == dataMap.end())
-                {
-                    Model* m = getModel();
-                    m->processSeed(10 + x * 10 + z, x, z);  
-                    dataMap[c] = m;
-                }
-                tempModels[c] = dataMap[c];
-            }
-        }
-
-        for (int x = cellx - radio; x <= cellx + radio; x++) {
-            for (int z = cellz - radio; z <= cellz + radio; z++) {
-                Coordenada coord = {x, z};
-                
-                if (mundialMap.find(coord) == mundialMap.end()) 
-                {
-                    if (tempModels.count({x+1, z}) && tempModels[coord]->doorData.count(0)) {
-                        DoorPoints p1 = tempModels[coord]->doorData[0], p2 = tempModels[{x+1, z}]->doorData[1];
-                        //Sumamos la escala a los vertices de la puerta del vecino 
-                        //Para trasladarla a su rejilla y sea local a la puerta origen
-                        //sin esto, los pasillos se generarían dentro de un mismo cuarto
-                        //joder sé que es difícil de visualizar, pero así funciona
-                        p2.infIzq.x += escala;
-                        p2.supIzq.x += escala;
-                        p2.infDer.x += escala;
-                        p2.supDer.x += escala;
-                        tempModels[coord]->generateCorridor(p1, p2);
-                    }
-                    if (tempModels.count({x, z+1}) && tempModels[coord]->doorData.count(2)) {
-                        DoorPoints p1 = tempModels[coord]->doorData[2], p2 = tempModels[{x, z+1}]->doorData[3];
-                        p2.infIzq.z += escala;
-                        p2.supIzq.z += escala; 
-                        p2.infDer.z += escala; 
-                        p2.supDer.z += escala;
-                        tempModels[coord]->generateCorridor(p1, p2);
-                    }
-
-                        tempModels[coord]->updatePointers();
-                        Paint* newRoom = getPaint();
-                        newRoom->configBuffers(*tempModels[coord]);
-                        mundialMap[coord] = newRoom;
-                }
-                mundialMap[coord]->draw(glm::vec3(x * escala, 0.0f, z * escala));
-            }
-        }
-    
-        static int cleanupCounter = 0;
-        if (++cleanupCounter > 60) 
-        {
-            cleanupCounter = 0;
-            for (auto it = mundialMap.begin(); it != mundialMap.end(); ) 
+    void manageMemory(int cellx, int cellz, int radio)
+    {
+        for (auto it = mundialMap.begin(); it != mundialMap.end(); ) 
             {
                 if (abs(it->first.x - cellx) > 3 || abs(it->first.z - cellz) > 3) 
                 {
@@ -196,11 +105,145 @@ public:
                         returnModel(dataMap[it->first]);
                         dataMap.erase(it->first);
                     }
-                    it = mundialMap.erase(it);
-                } else ++it;
+                        it = mundialMap.erase(it);
+                } 
+                else 
+                {
+                ++it;
+                }
             }
+    }
+
+    void clearChunk(int cellx, int cellz, int radio)
+    {
+        static int lastCellX = -99999, lastCellZ = -99999;
+                if (lastCellX != -99999) 
+                {
+                    if (cellx > lastCellX) 
+                    {
+                        int badX = lastCellX + radio;
+                        for (auto it = mundialMap.begin(); it != mundialMap.end(); ) 
+                        {
+                            if (it->first.x == badX) 
+                            {
+                                returnPaint(it->second);  
+                                if (dataMap.count(it->first)) 
+                                {
+                                    returnModel(dataMap[it->first]);  
+                                    dataMap.erase(it->first);
+                                }
+                                it = mundialMap.erase(it);
+                            } 
+                            else 
+                            {
+                                ++it;
+                            }
+                        }
+                    }
+
+                    if (cellz > lastCellZ) 
+                    {
+                        int badZ = lastCellZ + radio;
+                        for (auto it = mundialMap.begin(); it != mundialMap.end(); ) 
+                        {
+                            if (it->first.z == badZ) 
+                            {
+                                returnPaint(it->second); 
+                                if (dataMap.count(it->first)) 
+                                {
+                                    returnModel(dataMap[it->first]);
+                                    dataMap.erase(it->first);
+                                }
+                                it = mundialMap.erase(it);
+                            } 
+                            else 
+                            {
+                                ++it;
+                            }
+                        }
+                    }
+                }
+                lastCellX = cellx; lastCellZ = cellz;        
+    }
+
+    void create(int cellx, int cellz, int radio, float escala)
+    {
+        std::map<Coordenada, Model*> tempModels;
+        for (int x = cellx - radio; x <= cellx + radio; x++) {
+                    for (int z = cellz - radio; z <= cellz + radio; z++) {
+                        Coordenada c = {x, z};
+
+                        if (dataMap.find(c) == dataMap.end())
+                        {
+                            Model* m = getModel();
+                            m->processSeed(10 + x * 10 + z, x, z);  
+                            dataMap[c] = m;
+                        }
+                        tempModels[c] = dataMap[c];
+                    }
+                }
+
+                for (int x = cellx - radio; x <= cellx + radio; x++) {
+                    for (int z = cellz - radio; z <= cellz + radio; z++) {
+                        Coordenada coord = {x, z};
+                        
+                        if (mundialMap.find(coord) == mundialMap.end()) 
+                        {
+                            if (tempModels.count({x+1, z}) && tempModels[coord]->doorData.count(0)) {
+                                DoorPoints p1 = tempModels[coord]->doorData[0], p2 = tempModels[{x+1, z}]->doorData[1];
+                                //Sumamos la escala a los vertices de la puerta del vecino 
+                                //Para trasladarla a su rejilla y sea local a la puerta origen
+                                //sin esto, los pasillos se generarían dentro de un mismo cuarto
+                                //joder sé que es difícil de visualizar, pero así funciona
+                                p2.infIzq.x += escala;
+                                p2.supIzq.x += escala;
+                                p2.infDer.x += escala;
+                                p2.supDer.x += escala;
+                                tempModels[coord]->generateCorridor(p1, p2);
+                            }
+                            if (tempModels.count({x, z+1}) && tempModels[coord]->doorData.count(2)) 
+                            {
+                                DoorPoints p1 = tempModels[coord]->doorData[2], p2 = tempModels[{x, z+1}]->doorData[3];
+                                p2.infIzq.z += escala;
+                                p2.supIzq.z += escala; 
+                                p2.infDer.z += escala; 
+                                p2.supDer.z += escala;
+                                tempModels[coord]->generateCorridor(p1, p2);
+                            }
+
+                                tempModels[coord]->updatePointers();
+                                Paint* newRoom = getPaint();
+                                newRoom->configBuffers(*tempModels[coord], true);
+                                mundialMap[coord] = newRoom;
+                        }
+                        mundialMap[coord]->draw(glm::vec3(x * escala, 0.0f, z * escala));
+                    }
+                }        
+    }
+
+    void loadChunk(GLFWwindow *window, Camera &cam) {
+        float escala = 45.0f;
+        int cellx = (int)floor(cam.position.x / escala);
+        int cellz = (int)floor(cam.position.z / escala);
+        int radio = 2;
+
+        //limpiar cuartos que no aparecen para no renderizarlos
+        clearChunk(cellx, cellz, radio);
+        
+        //crear cuartos y pasillos
+        create(cellx, cellz, radio, escala);
+    
+        //limpiar memoria
+        static int cleanupCounter = 0;
+        if (++cleanupCounter > 60) 
+        {
+            cleanupCounter = 0;
+            manageMemory(cellx, cellz, 2);
+            
         }
     }
 };
+
+
 
 #endif
--- paint.h
+++ paint.h
@@ -25,15 +25,19 @@ class Paint {
             text = t;
         }
 
-        void configBuffers(Model &room) {
+        void configBuffers(Model &room, bool hasEbO) 
+        {
             glBindVertexArray(VAO);
 
             glBindBuffer(GL_ARRAY_BUFFER, VBO);
             glBufferData(GL_ARRAY_BUFFER, room.sizeVertices, room.vertices, GL_STATIC_DRAW);
 
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, room.sizeIndices, room.indices, GL_STATIC_DRAW);
-
+            if (hasEbO)
+            {
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, room.sizeIndices, room.indices, GL_STATIC_DRAW);
+            }
+            
             glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
             glEnableVertexAttribArray(0);
 
@@ -42,7 +46,7 @@ class Paint {
 
             cant = room.indicesAmount;
             glBindVertexArray(0); 
-        } 
+        }
 
         void draw(glm::vec3 pos, float rot = 0.0f) {
             glm::mat4 model = glm::mat4(1.0f);
